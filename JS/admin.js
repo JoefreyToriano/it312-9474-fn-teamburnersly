1a0795f4ac0ffcfadeeb28b1b8db155cbd388e23
Jermin Odcheo <2224981@slu.edu.ph>
2023-12-06 10:50:53 +0800

    Add filtering search for user management

--- JS/admin.js
+++ JS/admin.js
@@ -1,39 +1,77 @@
-function getUsers(){
-    var url ='../PHP/admin.php';
-    fetch(url).then(function(response){   
-        return response.text();
-    }).then(function(data){
-        users = JSON.parse(data);
-        console.log(users);
-        var div = document.createElement("div");
-        var p = document.createElement("p");
-        p.innerHTML = "First Name";
-        div.appendChild(p);
-        p = document.createElement("p");
-        p.innerHTML = "Last Name";
-        div.appendChild(p);
-        p = document.createElement("p");
-        p.innerHTML = "User Name";
-        div.appendChild(p);
-        p = document.createElement("p");
-        p.innerHTML = "Password";
-        div.appendChild(p);
-        p = document.createElement("p");
-        p.innerHTML = "Email";
-        div.appendChild(p);
-        p = document.createElement("p");
-        p.innerHTML = "User type";
-        div.appendChild(p);
-        document.getElementById("userPanel").appendChild(div);
-        users.forEach(user => {
+function getUsers(searchTerm = "", sortField = null, sortOrder = null) {
+  var url = "../PHP/admin.php";
+  fetch(url)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (data) {
+      var users = JSON.parse(data);
+
+      // Filter logic
+      if (searchTerm) {
+        users = users.filter(
+          (user) =>
+            user.fname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.lname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }
+
+      // Sorting logic
+      if (sortField && sortOrder) {
+        users.sort((a, b) => {
+          let valA = a[sortField].toLowerCase(); // Convert to lowercase for comparison
+          let valB = b[sortField].toLowerCase(); // Convert to lowercase for comparison
+
+          if (valA < valB) return sortOrder === "asc" ? -1 : 1;
+          if (valA > valB) return sortOrder === "asc" ? 1 : -1;
+          return 0;
+        });
+      }
+
+      updateUserPanel(users, sortField, sortOrder);
+    });
+}
+
+function updateUserPanel(users, sortField = null, sortOrder = "asc") {
+  const userPanel = document.getElementById("userPanel");
+  userPanel.innerHTML = ""; // Clear existing content
+
+  // Create header row
+  var headerDiv = document.createElement("div");
+  var headers = {
+    fname: "First Name",
+    lname: "Last Name",
+    username: "User Name",
+    password: "Password",
+    email: "Email",
+    usertype: "User Type",
+  };
+
+  Object.keys(headers).forEach((key) => {
+    var p = document.createElement("p");
+    p.innerHTML = headers[key];
+    if (key === sortField && sortOrder) {
+      p.innerHTML += sortOrder === "asc" ? " ↑" : " ↓";
+    }
+    p.onclick = () => sortUsers(key);
+    headerDiv.appendChild(p);
+  });
+  userPanel.appendChild(headerDiv);
+
+  // Create and append userDiv with user details
+  users.forEach((user) => {
     var userDiv = document.createElement("div");
 
     // Append user details
-    ['fname', 'lname', 'username', 'password', 'email', 'usertype'].forEach(detail => {
+    ["fname", "lname", "username", "password", "email", "usertype"].forEach(
+      (detail) => {
         var p = document.createElement("p");
         p.innerHTML = user[detail];
         userDiv.appendChild(p);
-    });
+      }
+    );
 
     // Container for action icons
     var actionsContainer = document.createElement("div");
@@ -55,10 +93,37 @@ function getUsers(){
     // Append actions container to the userDiv
     userDiv.appendChild(actionsContainer);
 
-    document.getElementById("userPanel").appendChild(userDiv);
-        });
-        
-    })
+    userPanel.appendChild(userDiv);
+  });
 }
 
-getUsers();
+function applySearch() {
+  var searchTerm = document.getElementById("searchInput").value;
+  getUsers(searchTerm);
+}
+var currentSortState = {
+  fname: null,
+  lname: null,
+  username: null,
+  email: null,
+  usertype: null,
+};
+function sortUsers(sortField) {
+  var states = [null, "asc", "desc"];
+  var currentIndex = states.indexOf(currentSortState[sortField]);
+  var nextStateIndex = (currentIndex + 1) % states.length;
+  currentSortState[sortField] = states[nextStateIndex];
+
+  // Reset other fields to null
+  Object.keys(currentSortState).forEach((key) => {
+    if (key !== sortField) currentSortState[key] = null;
+  });
+
+  getUsers("", sortField, currentSortState[sortField]);
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("searchButton")
+    .addEventListener("click", applySearch);
+  getUsers();
+});
